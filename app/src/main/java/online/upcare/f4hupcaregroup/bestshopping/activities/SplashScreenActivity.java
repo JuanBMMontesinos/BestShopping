e4f8f514a6c13fc6da40f5e81c6335418a520ea9
JuanBMMontesinos <juan.m.montesinos@gmail.com>
2017-01-24 12:39:51 -0200

    .
    .

---
+++ app/src/main/java/online/upcare/f4hupcaregroup/bestshopping/activities/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package online.upcare.f4hupcaregroup.bestshopping.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SplashScreenActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
